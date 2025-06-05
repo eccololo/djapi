36a7e876bcb58ff5480016403778673312c44305
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2025-06-05 17:22:40 +0200

    Add Movie and Rating ViewSets to views.py.

--- movierater/api/urls.py
+++ movierater/api/urls.py
@@ -3,7 +3,11 @@ from django.conf.urls import include
 
 from rest_framework import routers
 
+from .views import MovieViewSet, RatingViewSet
+
 router = routers.DefaultRouter()
+router.register("movies", MovieViewSet)
+router.register("ratings", RatingViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
